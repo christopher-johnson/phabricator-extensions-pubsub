b5491a0999cfe968e9f9f210168859ca411257b8
Christopher Johnson <christopher.johnson@wikimedia.de>
2015-08-05 11:55:43 +0200

    removes pubsub:event custom field from property display

--- src/customfield/PubHubEventField.php
+++ src/customfield/PubHubEventField.php
@@ -26,4 +26,9 @@ final class PubSubEventField extends PubSubProjectCustomField {
     return $this->renderTextProxyPropertyViewValue($this->textproxy, $handles);
   }
 
+  // == Search
+  public function shouldAppearInPropertyView() {
+    return false;
+  }
+
 }
